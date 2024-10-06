f3ced8e63d961acf724e3022ae15b465d83f706b
juke86 <justinkennedy86@gmail.com>
2024-10-06 18:13:57 -0400

    fart

--- main.tf
+++ main.tf
@@ -1,19 +1,26 @@
  resource "aws_instance" "ec2-instance1" {
-   ami = "ami-0005e0cfe09cc9050"
-   instance_type = "t2.micro"
+   ami = "ami-09da212cf18033880" # AMI for us-east-2
+   instance_type = "t2.micro" #free
 
    tags = {                                
      Name = "demo-server1"
 }
 }
-
-
 resource "aws_instance" "ec2-Instance2" {
   provider      = aws.west
-  ami           = "ami-0a5ed7a812aeb495a"  # AMI for us-west-1
-  instance_type = "t2.micro"
+  ami           = "ami-0d081196e3df05f4d"  # AMI for us-west-2
+  instance_type = "t2.micro" #free
 
   tags = {                                
     Name = "demo-server2"
+}
+}
+resource "aws_instance" "ec2-Instance3" {
+  provider      = aws.aussie
+  ami           = "ami-0cf70e1d861e1dfb8"  # AMI for ap-southeast-2
+  instance_type = "t2.micro" # free
+
+  tags = {                                
+    Name = "demo-server3"
 }
 }
