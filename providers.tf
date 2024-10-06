e374c6b6ded5dbee33b4a5c547dfdd17cd2dd1ab
juke86 <justinkennedy86@gmail.com>
2024-10-06 18:15:01 -0400

    fart

--- providers.tf
+++ providers.tf
@@ -2,20 +2,24 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 5.31.0"
+      version = "5.31.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  profile = "Helen"
+  region = "us-east-2"
+  profile = "jkennedy"
   # Configuration options
 }
-
 provider "aws" {
   alias = "west"
-  region = "us-west-1"
-  profile = "Helen"
+  region = "us-west-2"
+  profile = "jkennedy"
+  # Configuration options
 }
-
+provider "aws" {
+  alias = "aussie"
+  region = "ap-southeast-2"
+  profile = "jkennedy"
+}
